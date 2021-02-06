a7fb74166bb2fc45b28930bc3d86776f84deab13
rkreddy99 <krishna.reddy@iitgn.ac.in>
2021-02-06 22:18:38 +0530

    Initial Commit

---
+++ pages/faculty.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function faculty() {
+  return <div></div>;
+}
+
+export default faculty;
---
+++ pages/student.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function student() {
+  return <div></div>;
+}
+
+export default student;
